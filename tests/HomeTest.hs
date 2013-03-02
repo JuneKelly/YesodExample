e6d83b762b6275996998d0d3e876ade7063b8000
Shane Kilkelly <shanekilkelly@gmail.com>
2013-03-02 15:20:35 +0000

    basic homepage test

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -7,10 +7,15 @@ import TestImport
 import TestTools
 
 homeSpecs :: Specs
-homeSpecs =
-  describe "The Homepage" $
+homeSpecs = describe "The Homepage" $ do
     it "should be shown at site root" $ do
       get_ "/"
       statusIs 200
       htmlAllContain "h1" "Welcome"
-      htmlAllContain "p"  "This is an example of Yesod in action" 
+      htmlAllContain "p"  "This is an example of Yesod in action"
+      
+    it "should have appropriate menu items" $ do
+      get_ "/"
+      statusIs 200
+      bodyContains "Home"
+      bodyContains "Login"
