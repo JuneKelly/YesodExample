e6c6f5852108a37ad75fa13ded55185a3048d556
Shane Kilkelly <shanekilkelly@gmail.com>
2013-03-05 21:20:50 +0000

    TodoListTest not passing

--- tests/TodoListTest.hs
+++ tests/TodoListTest.hs
@@ -5,9 +5,18 @@ module TodoListTest
 
 import TestImport
 import TestTools
+import Yesod.Auth.HashDB (setPassword)
+import Database.Persist
+import Model
 
 todoListSpecs :: Specs
 todoListSpecs = describe "Main Lists Page" $ do
   
   it "requires login" $ do
     needsLogin GET "/lists"
+    
+  it "shows all todo lists" $ do
+    doLogin "testuser" "password"
+    get_ "/lists"
+    statusIs 200
+    htmlAllContain "h1" "Your Todo Lists"
