926f39dab6c9a1a66674754b0e85fcb1e9eac313
Shane Kilkelly <shanekilkelly@gmail.com>
2013-03-09 18:48:02 +0000

    will revisit

--- tests/TodoListTest.hs
+++ tests/TodoListTest.hs
@@ -6,15 +6,39 @@ module TodoListTest
 import TestImport
 import TestTools
 import Yesod.Auth.HashDB (setPassword)
+import Import hiding (runDB)
+import Data.ByteString.Char8 (pack)
+
+import System.IO (hPutStrLn, stderr)
 
 todoListSpecs :: Specs
-todoListSpecs = describe "Main Lists Page" $ do
+todoListSpecs = do
+  describe "Main Lists Page" $ do
   
-  it "requires login" $ do
-    needsLogin GET "/lists"
+    it "requires login" $ do
+      needsLogin GET "/lists"
+      
+    it "shows all todo lists" $ do
+      doLogin "testuserone" "password"
+      get_ "/lists"
+      statusIs 200
+      htmlAllContain "h1" "Your Todo Lists"
+{-
+  describe "List page" $ do
     
-  it "shows all todo lists" $ do
-    doLogin "testuserone" "password"
-    get_ "/lists"
-    statusIs 200
-    htmlAllContain "h1" "Your Todo Lists"
+    it "should show a list of tasks" $ do
+      doLogin "testuserone" "password"
+      mUser <- runDB $ selectFirst 
+                         [UserUsername ==. "testuserone"] []
+      case mUser of
+        Just (Entity k v) -> do
+          todoList <- runDB $ selectFirst [TodoListOwner ==. k] []
+          case todoList of
+            Just (Entity k v) -> do
+              let route = "/list/" ++ fromPersistValue $ unKey k
+              liftIO $ hPutStrLn stderr $ k
+              get_ $ pack $ route
+              statusIs 200
+            _ -> assertFailure "no list"
+        _ -> do assertFailure "no user"
+-}
