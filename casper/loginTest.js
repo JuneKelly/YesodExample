2599b77cc29804a0644cd5590fcbb4ba734e85e4
Shane Kilkelly <shanekilkelly@gmail.com>
2013-03-10 16:12:06 +0000

    test logins

--- casper/loginTest.js
+++ casper/loginTest.js
@@ -7,10 +7,24 @@ casper.start('http://localhost:3000/', function() {
     this.then(function() {
         this.test.assertExists('form', 'login form exists');
         this.test.assertUrlMatch('http://localhost:3000/auth/login');
+
+        this.fill('form', {
+            'username' : 'testuserone',
+            'password' : 'password'
+        }, true);
+        this.then(function() {
+
+            this.test.assertExists('div .alert', 'alert exists');
+            this.test.assertSelectorHasText('div .alert', 'Logged in');
+            
+            this.test.assertSelectorHasText('a', 'Logout');
+            this.test.assertSelectorDoesntHaveText('a', 'Login');
+
+        });
     });
 
 });
 
 casper.run(function() {
-    this.test.done(2);
+    this.test.done(6);
 });
